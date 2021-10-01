66a8fc6fd7859729000c6a14352d8cda5348cc2c
rohithdinesan <91694756+rohithdinesan@users.noreply.github.com>
2021-10-01 22:21:43 +0530

    updated some codes

--- Java/Test.java
+++ Java/Test.java
@@ -1,11 +1,14 @@
 import java.io.*;
+import java.util.*;
 
 // Java program for implementation of Bubble Sort 
 class Test
 { 
     void bubbleSort(int arr[]) 
     { 
-        int n = arr.length; 
+        int n = arr.length;
+        int l = arr.length + 1;
+        
         for (int i = 0; i < n-1; i++) 
             for (int j = 0; j < n-i-1; j++) 
                 if (arr[j] > arr[j+1]) 
@@ -23,14 +26,14 @@ class Test
         int n = arr.length; 
         for (int i=0; i<n; ++i) 
             System.out.print(arr[i] + " "); 
-        System.out.println(); 
+        System.out.print("\n"); 
     } 
   
     // Driver method to test above 
     public static void main(String args[]) 
     { 
         BubbleSort ob = new BubbleSort(); 
-        int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+        int arr[] = {64, 34, 25, 12, 22, 11, 90, 55}; 
         ob.bubbleSort(arr); 
         System.out.println("Sorted array"); 
         ob.printArray(arr); 
