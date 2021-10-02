618deadb080e1f83cd28ee578c0ecbb10b4c0857
GayathriKReghu <90635296+GayathriKReghu@users.noreply.github.com>
2021-10-02 08:29:26 +0530

    Update Bubble.java

--- Java/Bubble.java
+++ Java/Bubble.java
@@ -1,12 +1,71 @@
 import java.io.*;
-
-class Bubble 
+class Sort
 {
-    void main()
-    {
-        BufferedInputStream obj= new BufferedInputStream(in);
-        int i=integer.parseint(in.readLine());
-        
-        System.out.println("i = " + i);
-    }
+private int max;
+private int i;
+public int[] P;
+public Sort(int S)
+{
+max=S;
+P=new int[max];
+}
+
+public  void BubbleSort()
+{
+    
+int j,temp;
+for(i=0;i<max-1;i++)
+{
+    System.out.println("hai");
+for(j=0;j<max-i-1;j++)
+{
+if(P[j]>P[j+1])
+{
+temp=P[j];
+P[j]=P[j+1];
+P[j+1]=temp;
+
+}
+}
+}
+
+}
+
+public  void Display()
+{
+
+for(i=0;i<max;i++)
+{
+System.out.println(P[i]+" is the array element at : "+ i +"\n");
+}
+}
+
+
+
+public static void main(String args[]) throws IOException
+{
+int n;
+System.out.println("Enter the array size ");
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+n=Integer.parseInt(br.readLine());
+Sort A= new Sort(n);
+
+
+System.out.println("Enter the array elements : ");
+int k;
+for( k=0;k<n;k++)
+{
+System.out.println("Enter the array element at position  ");
+
+A.P[k]=Integer.parseInt(br.readLine());
+
+}
+
+
+
+A.BubbleSort();
+A.Display();
+
+
+}
 }
